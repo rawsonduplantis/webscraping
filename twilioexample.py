6a103cad6f2b3be50d0022e4216221914170892c
Rawson Duplantis <46237409+ishkena@users.noreply.github.com>
2023-05-05 20:30:13 -0500

    edits

--- twilioexample.py
+++ twilioexample.py
@@ -3,8 +3,8 @@ from twilio.rest import Client
 
 client = Client(keys.account_sid, keys.auth_token)
 
-twilio_num = '+19379322599'
-my_cell = '+12818258833'
+twilio_num = ''
+my_cell = ''
 
 text_message = client.messages.create(to=my_cell, from_=twilio_num, body="Test text")
 
